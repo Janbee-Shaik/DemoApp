9de7061fce65f8e37bfa1973ee4ae63c96daa1f9
Janbee-Shaik <janbee.shaik05@gmail.com>
2022-02-21 16:21:04 +0530

    testing

--- src/main/java/com/example/SampleAppApplication.java
+++ src/main/java/com/example/SampleAppApplication.java
@@ -1,4 +1,4 @@
-package com.example;
+ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +8,6 @@ public class SampleAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleAppApplication.class, args);
-		System.out.println("*******Hello World********");
+		System.out.println("*******Hi! Hello World********");
 	}
 }
